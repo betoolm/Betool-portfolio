fe5aef3d5819b5c0b59615b58a8bd2348d8e46ec
betoolm <150656430+betoolm@users.noreply.github.com>
2025-02-23 00:51:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ function showMedia(projectId) {
     }
 }
 
-// Smooth scrolling for nav links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
